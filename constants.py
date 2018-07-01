6db1609402b5dd2e254fdfb4fea419642878cb48
Mcliuyi <37773013+Mcliuyi@users.noreply.github.com>
2018-07-01 12:38:45 +0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,8 +1,8 @@
 
 # QQ登录参数
-QQ_APP_ID = '101474184'
-QQ_APP_KEY = 'c6ce949e04e12ecc909ae6a8b09b637c'
-QQ_REDIRECT_URI = 'http://www.meiduo.site:8080/oauth_callback.html'
+QQ_APP_ID = 'xxxxx'
+QQ_APP_KEY = 'xxxxxx'
+QQ_REDIRECT_URI = 'xxx' # 用户QQ登陆后的回调地址
 QQ_URL = 'https://graph.qq.com/oauth2.0/me?access_token='
 # QQ用户绑定帐号token有效期
 SAVE_QQ_TOKEN_EXPRIES = 60 * 60 * 2
